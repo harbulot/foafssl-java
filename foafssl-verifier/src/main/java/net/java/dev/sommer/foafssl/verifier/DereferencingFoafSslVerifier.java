decbf582496564c02c17063c5cc0c0cc49f48f2a
harbulot <harbulot@a925da88-a912-0410-a1af-dd118eaf9e3a>
2009-03-20 16:36:11 +0000

    Created foafssl-verifier module (mainly from CheckClient) and added unit tests.
    git-svn-id: https://sommer.dev.java.net/svn/sommer/maven/foafssl@465 a925da88-a912-0410-a1af-dd118eaf9e3a

---
+++ foafssl-verifier/src/main/java/net/java/dev/sommer/foafssl/verifier/DereferencingFoafSslVerifier.java
@@ -0,0 +1,249 @@
+/*
+New BSD license: http://opensource.org/licenses/bsd-license.php
+
+Copyright (c) 2008 Sun Microsystems, Inc.
+901 San Antonio Road, Palo Alto, CA 94303 USA. 
+All rights reserved.
+
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+- Neither the name of Sun Microsystems, Inc. nor the names of its contributors
+may be used to endorse or promote products derived from this software 
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.java.dev.sommer.foafssl.verifier;
+
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.security.PublicKey;
+import java.util.logging.Logger;
+import java.security.cert.X509Certificate;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.URISyntaxException;
+import java.net.URI;
+import java.net.URL;
+import java.security.interfaces.DSAPublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.openrdf.OpenRDFException;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.query.Binding;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.sail.memory.MemoryStore;
+
+/**
+ * @author Henry Story.
+ * @author Bruno Harbulot.
+ */
+public class DereferencingFoafSslVerifier {
+	static transient Logger log = Logger
+			.getLogger(DereferencingFoafSslVerifier.class.getName());
+
+	public Collection<URI> verifyFoafSslCertificate(X509Certificate clientCert)
+			throws OpenRDFException, IOException {
+		List<URI> verifiedUris = new ArrayList<URI>();
+		List<URI> candidateUris = getAlternativeURIName(clientCert);
+
+		for (URI candidateUri : candidateUris) {
+			if (verifyByDereferencing(candidateUri, clientCert.getPublicKey())) {
+				verifiedUris.add(candidateUri);
+			}
+		}
+		return verifiedUris;
+	}
+
+	public boolean verifyByDereferencing(URI claimedIdUri,
+			PublicKey certPublicKey) throws OpenRDFException, IOException {
+		URL foafname = claimedIdUri.toURL();
+		URLConnection conn = foafname.openConnection();
+
+		conn.addRequestProperty("Accept:", "application/rdf+xml, text/rdf+n3");
+		conn.connect();
+
+		InputStream is = conn.getInputStream();
+		try {
+			return verifyByDereferencing(claimedIdUri, certPublicKey, conn
+					.getURL(), is, conn.getContentType());
+		} finally {
+			is.close();
+		}
+	}
+
+	public boolean verifyByDereferencing(URI claimedIdUri,
+			PublicKey certPublicKey, URL actualUrl,
+			InputStream foafDocInputStream, String foafMediaType)
+			throws OpenRDFException, IOException {
+		RDFFormat rdfFormat = RDFFormat.forMIMEType(foafMediaType);
+
+		URL base = new URL(actualUrl.getProtocol(), actualUrl.getHost(),
+				actualUrl.getPort(), actualUrl.getFile()); // all of this needs
+
+		MemoryStore mem = new MemoryStore();
+		mem.initialize();
+		SailRepository sail = new SailRepository(mem);
+		RepositoryConnection rep = sail.getConnection();
+		ValueFactory vf = sail.getValueFactory();
+
+		// to be better
+		org.openrdf.model.URI foafdocUri = vf.createURI(base.toString());
+		rep
+				.add(foafDocInputStream, actualUrl.toString(), rdfFormat,
+						foafdocUri);
+		if (certPublicKey instanceof RSAPublicKey) {
+			RSAPublicKey certRsakey = (RSAPublicKey) certPublicKey;
+			TupleQuery query = rep
+					.prepareTupleQuery(
+							QueryLanguage.SPARQL,
+							"PREFIX cert: <http://www.w3.org/ns/auth/cert#>"
+									+ "PREFIX rsa: <http://www.w3.org/ns/auth/rsa#>"
+									+ "SELECT ?mod ?exp "
+									+ "WHERE {"
+									+ "   ?sig cert:identity ?person ."
+									+ "   ?sig a rsa:RSAPublicKey;"
+									+ "        rsa:modulus [ cert:hex ?mod ] ;"
+									+ "        rsa:public_exponent [ cert:decimal ?exp ] ."
+									+ "}");
+			// TODO: allow optional different ways of encoding the
+			// modulus and exponent integers
+			// this would just require passing the relations and the
+			// value to a function.
+			query.setBinding("person", vf.createURI(claimedIdUri.toString()));
+			TupleQueryResult answer = query.evaluate();
+			while (answer.hasNext()) {
+				BindingSet binding = answer.next();
+				Binding value = binding.getBinding("mod");
+				if (value != null) {
+					// check that the value and type match the one
+					// on the signature
+					String strval = cleanHex(value.getValue().stringValue());
+					BigInteger foaf_modulus = new BigInteger(strval, 16);
+					if (!foaf_modulus.equals(certRsakey.getModulus())) {
+						continue;
+					}
+				} else
+					continue;
+
+				value = binding.getBinding("exp");
+				if (value != null) {
+					BigInteger exponent = new BigInteger(value.getValue()
+							.stringValue(), 10);
+					if (!exponent.equals(certRsakey.getPublicExponent())) {
+						continue;
+					}
+				} else
+					continue;
+
+				// success!
+				return true;
+			}
+		} else if (certPublicKey instanceof DSAPublicKey) {
+
+		} else {
+			// what else ?
+		}
+		return false;
+	}
+
+	/**
+	 * we are interested in the alternative URI names in the certificates
+	 * (perhaps others such as email addresses could also be useful)
+	 * 
+	 * @param cert
+	 * @return a list of such URIs
+	 */
+	public static List<URI> getAlternativeURIName(X509Certificate cert) {
+		ArrayList<URI> answers = new ArrayList<URI>();
+		try {
+			if (cert == null) {
+				return answers;
+			}
+			Collection<List<?>> names = cert.getSubjectAlternativeNames();
+			if (names == null) {
+				// beurk! this is part of the spec. it can return null
+				return answers;
+			}
+			for (Iterator<List<?>> it = names.iterator(); it.hasNext();) {
+				List<?> altNameList = it.next();
+				Integer id = (Integer) altNameList.get(0);
+				if (id == 6) { // see X509 spec, section 8.3.2.1 these are the
+					// URIs!
+					Object uristr = altNameList.get(1);
+					if (uristr instanceof String) {
+						try {
+							URI foafid = new URI((String) uristr);
+							answers.add(foafid);
+						} catch (URISyntaxException e) {
+							e.printStackTrace();
+						}
+					} else {
+						// if we are to return other things, such as email and
+						// so we would need a more
+						// complex structure in the return, perhaps even a class
+						// for the X509 cert with
+						// more helpful return methods
+					}
+				}
+			}
+		} catch (java.security.cert.CertificateParsingException e) {
+			e.printStackTrace(); // todo: decide what exception to throw
+		}
+		return answers;
+	}
+
+	static final private char[] hexchars = { '0', '1', '2', '3', '4', '5', '6',
+			'7', '8', '9', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e',
+			'F', 'f' };
+
+	static {
+		Arrays.sort(hexchars);
+	}
+
+	/**
+	 * This takes any string and returns in order only those characters that are
+	 * part of a hex string
+	 * 
+	 * @param strval
+	 *            any string
+	 * @return a pure hex string
+	 */
+	private String cleanHex(String strval) {
+		StringBuffer cleanval = new StringBuffer();
+		for (char c : strval.toCharArray()) {
+			if (Arrays.binarySearch(hexchars, c) >= 0) {
+				cleanval.append(c);
+			}
+		}
+		return cleanval.toString();
+	}
+}
---
+++ foafssl-verifier/src/test/java/net/java/dev/sommer/foafssl/verifier/DereferencingFoafSslVerifierTest.java
@@ -0,0 +1,120 @@
+/*
+New BSD license: http://opensource.org/licenses/bsd-license.php
+
+Copyright (c) 2008 Sun Microsystems, Inc.
+901 San Antonio Road, Palo Alto, CA 94303 USA. 
+All rights reserved.
+
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+- Neither the name of Sun Microsystems, Inc. nor the names of its contributors
+may be used to endorse or promote products derived from this software 
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.java.dev.sommer.foafssl.verifier;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.security.Security;
+import java.security.cert.X509Certificate;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMReader;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Bruno Harbulot.
+ */
+public class DereferencingFoafSslVerifierTest {
+	public static final URI BRUNO_FOAF_URI = URI
+			.create("http://www.harbulot.com/foaf/bruno#me");
+	public static final URI BRUNO_FOAF_DOC_URI = URI
+			.create("http://www.harbulot.com/foaf/bruno");
+
+	public static final String TEST_BRUNO_FOAF_FILENAME = "bruno.rdf.xml";
+	public static final String TEST_BRUNO_WRONG_FOAF_FILENAME = "bruno-wrong.rdf.xml";
+	public static final String TEST_BRUNO_CERT_FILENAME = "bruno-foafssl.pem";
+
+	private DereferencingFoafSslVerifier verifier;
+	private X509Certificate x509Certificate;
+
+	@Before
+	public void setUp() throws Exception {
+		Security.addProvider(new BouncyCastleProvider());
+		this.verifier = new DereferencingFoafSslVerifier();
+
+		InputStreamReader certReader = new InputStreamReader(
+				DereferencingFoafSslVerifierTest.class
+						.getResourceAsStream(TEST_BRUNO_CERT_FILENAME));
+
+		PEMReader pemReader = new PEMReader(certReader);
+		while (pemReader.ready()) {
+			Object pemObject = pemReader.readObject();
+			if (pemObject instanceof X509Certificate) {
+				x509Certificate = (X509Certificate) pemObject;
+				break;
+			} else {
+				throw new RuntimeException("Unknown type of PEM object: "
+						+ pemObject);
+			}
+		}
+		pemReader.close();
+	}
+
+	@Test
+	public void testGoodLocalFoafFile() throws Exception {
+
+		InputStream foafInputStream = DereferencingFoafSslVerifierTest.class
+				.getResourceAsStream(TEST_BRUNO_FOAF_FILENAME);
+
+		try {
+			assertTrue(this.verifier.verifyByDereferencing(BRUNO_FOAF_URI,
+					this.x509Certificate.getPublicKey(), BRUNO_FOAF_DOC_URI
+							.toURL(), foafInputStream, "application/rdf+xml"));
+		} finally {
+			foafInputStream.close();
+		}
+	}
+
+	@Test
+	public void testBadLocalFoafFile() throws Exception {
+		InputStream foafInputStream = DereferencingFoafSslVerifierTest.class
+				.getResourceAsStream(TEST_BRUNO_WRONG_FOAF_FILENAME);
+		try {
+			assertFalse(this.verifier.verifyByDereferencing(BRUNO_FOAF_URI,
+					this.x509Certificate.getPublicKey(), BRUNO_FOAF_DOC_URI
+							.toURL(), foafInputStream, "application/rdf+xml"));
+		} finally {
+			foafInputStream.close();
+		}
+	}
+
+	@Test
+	public void testRemoteFoafFile() throws Exception {
+		assertTrue(this.verifier.verifyByDereferencing(BRUNO_FOAF_URI,
+				this.x509Certificate.getPublicKey()));
+	}
+}
