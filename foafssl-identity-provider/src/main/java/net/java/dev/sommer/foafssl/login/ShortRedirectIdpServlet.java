a2bb575a9174e79fced7243bc96fc3ae434ff656
Henry J. Story <henry.story@bblfish.net>
2010-03-07 21:04:39 +0100

    The Redirect Servlet was not redirecting on broken foaf files

--- foafssl-identity-provider/src/main/java/net/java/dev/sommer/foafssl/login/ShortRedirectIdpServlet.java
+++ foafssl-identity-provider/src/main/java/net/java/dev/sommer/foafssl/login/ShortRedirectIdpServlet.java
@@ -1,33 +1,33 @@
 /**-----------------------------------------------------------------------
-  
-Copyright (c) 2009-2010, The University of Manchester, United Kingdom.
-All rights reserved.
 
-Redistribution and use in source and binary forms, with or without 
-modification, are permitted provided that the following conditions are met:
+ Copyright (c) 2009-2010, The University of Manchester, United Kingdom.
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
 
  * Redistributions of source code must retain the above copyright notice, 
-      this list of conditions and the following disclaimer.
+ this list of conditions and the following disclaimer.
  * Redistributions in binary form must reproduce the above copyright 
-      notice, this list of conditions and the following disclaimer in the 
-      documentation and/or other materials provided with the distribution.
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
  * Neither the name of the The University of Manchester nor the names of 
-      its contributors may be used to endorse or promote products derived 
-      from this software without specific prior written permission.
+ its contributors may be used to endorse or promote products derived
+ from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
 
------------------------------------------------------------------------*/
+ -----------------------------------------------------------------------*/
 package net.java.dev.sommer.foafssl.login;
 
 import java.io.IOException;
@@ -61,7 +61,6 @@ import org.bouncycastle.util.encoders.Base64;
 /**
  * @author Bruno Harbulot (Bruno.Harbulot@manchester.ac.uk)
  * @author Henry Story
- * 
  */
 public class ShortRedirectIdpServlet extends AbstractIdpServlet {
     private static final long serialVersionUID = 2L;
@@ -131,53 +130,61 @@ public class ShortRedirectIdpServlet extends AbstractIdpServlet {
         X509Certificate[] certificates = (X509Certificate[]) request
                 .getAttribute("javax.servlet.request.X509Certificate");
 
-        if (certificates != null) {
-            X509Certificate foafSslCertificate = certificates[0];
+        if (certificates == null || certificates.length == 0) {
+            if (replyTo != null) {
+                redirect(response, replyTo + "?" + ERROR_PARAMNAME + "=nocert");
+                return;
+            }
+        } else {
+            X509Certificate foafSslCertificate = certificates[0]; //TODO: what about the other certs? should one iterate?
             try {
                 verifiedWebIDs = FOAF_SSL_VERIFIER.verifyFoafSslCertificate(foafSslCertificate);
             } catch (Exception e) {
                 if (replyTo != null) {
                     redirect(response, replyTo + "?" + ERROR_PARAMNAME + "="
                             + URLEncoder.encode(e.getMessage(), "UTF-8"));
+                    return;
                 } else {
                     // TODO error message
+                }
+            }
+            if (replyTo != null) {
+                if ((verifiedWebIDs != null) && (verifiedWebIDs.size() > 0)) {
+                    try {
+                        String authnResp = createSignedResponse(verifiedWebIDs, replyTo);
+                        redirect(response, authnResp);
+
+                    } catch (InvalidKeyException e) {
+                        LOG.log(Level.SEVERE, "Error when signing the response.", e);
+                        redirect(response, replyTo + "?" + ERROR_PARAMNAME + "=IdPError");
+                    } catch (NoSuchAlgorithmException e) {
+                        LOG.log(Level.SEVERE, "Error when signing the response.", e);
+                        redirect(response, replyTo + "?" + ERROR_PARAMNAME + "=IdPError");
+                    } catch (SignatureException e) {
+                        LOG.log(Level.SEVERE, "Error when signing the response.", e);
+                        redirect(response, replyTo + "?" + ERROR_PARAMNAME + "=IdPError");
+                    }
+                } else {
+                    redirect(response, replyTo + "?" + ERROR_PARAMNAME + "=noWebID");
                 }
                 return;
             }
         }
+        //anything that falls through to here, we show the help page
 
-        if ((replyTo != null) && (verifiedWebIDs != null) && (verifiedWebIDs.size() > 0)) {
-            try {
-                String authnResp = createSignedResponse(verifiedWebIDs, replyTo);
-                redirect(response, authnResp);
-            } catch (InvalidKeyException e) {
-                LOG.log(Level.SEVERE, "Error when signing the response.", e);
-                redirect(response, replyTo + "?" + ERROR_PARAMNAME + "=IdPError");
-            } catch (NoSuchAlgorithmException e) {
-                LOG.log(Level.SEVERE, "Error when signing the response.", e);
-                redirect(response, replyTo + "?" + ERROR_PARAMNAME + "=IdPError");
-            } catch (SignatureException e) {
-                LOG.log(Level.SEVERE, "Error when signing the response.", e);
-                redirect(response, replyTo + "?" + ERROR_PARAMNAME + "=IdPError");
-            }
-        } else {
-            request.setAttribute(AbstractIdpServlet.VERIFIED_WEBID_PRINCIPALS_REQATTR,
-                    verifiedWebIDs);
-            response.setContentType("text/html");
-            RequestDispatcher requestDispatcher = request
-                    .getRequestDispatcher(shortRedirectInclude);
-            requestDispatcher.include(request, response);
-        }
+        request.setAttribute(AbstractIdpServlet.VERIFIED_WEBID_PRINCIPALS_REQATTR,
+                verifiedWebIDs);
+        response.setContentType("text/html");
+        RequestDispatcher requestDispatcher = request
+                .getRequestDispatcher(shortRedirectInclude);
+        requestDispatcher.include(request, response);
+
     }
 
     /**
-     * 
-     * 
-     * @param verifiedWebIDs
-     *            a list of webIds identifying the user (only the fist will be
-     *            used)
-     * @param simpleRequestParam
-     *            the service that the response is sent to
+     * @param verifiedWebIDs     a list of webIds identifying the user (only the fist will be
+     *                           used)
+     * @param simpleRequestParam the service that the response is sent to
      * @return the URL of the response with the webid, timestamp appended and
      *         signed
      * @throws NoSuchAlgorithmException
@@ -185,8 +192,9 @@ public class ShortRedirectIdpServlet extends AbstractIdpServlet {
      * @throws InvalidKeyException
      * @throws SignatureException
      */
+
     private String createSignedResponse(Collection<? extends FoafSslPrincipal> verifiedWebIDs,
-            String simpleRequestParam) throws NoSuchAlgorithmException,
+                                        String simpleRequestParam) throws NoSuchAlgorithmException,
             UnsupportedEncodingException, InvalidKeyException, SignatureException {
         /*
          * Reads the FoafSsl simple authn request.
@@ -221,9 +229,9 @@ public class ShortRedirectIdpServlet extends AbstractIdpServlet {
 
     /**
      * Redirect request to the given url
-     * 
+     *
      * @param response
-     * @param respUrl the response Url to redirect to
+     * @param respUrl  1the response Url to redirect to
      */
     private void redirect(HttpServletResponse response, String respUrl) {
         response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
